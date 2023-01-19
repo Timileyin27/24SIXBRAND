26239f340670efd813ce1b9701a867cca32e63dc
raji2004 <kingraj1344@gmail.com>
2023-01-19 21:51:26 +0100

    done

---
+++ login.js
@@ -0,0 +1,20 @@
+const submitBtn = document.getElementById('submit')
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const storedUsername = localStorage.getItem('username')
+    const storedPassword = localStorage.getItem('password')
+
+    if(!username.value || !password.value){
+        alert("Please enter a username or password")
+    } else if((username.value !== storedUsername)||(password.value !== storedPassword)){
+        alert("Incorrect username or password")
+    } else if(storedPassword === null || storedUsername === null) {
+        alert("Your account does not exist")
+    }
+    else {
+        window.location.pathname = "/24SIXBRAND/index.html";
+    }
+})
